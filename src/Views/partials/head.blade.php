9a18f8d29e208a2c415e106e5be8dbff2a78152a
Jay <me@jirayu.in.th>
2020-02-05 02:26:00 +0700

    add Bootstrap

---
+++ src/Views/partials/head.blade.php
@@ -0,0 +1,6 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>Document</title>
+<script defer src="dist/scripts/main.js"></script>
+<link rel="stylesheet" href="dist/styles/main.css">
